508fdab92c207f46f02f8ca4a4c6eb9de1018138
2023Olga <borodaolga087@gmail.com>
2024-02-22 10:44:47 -0500

    autoFill.cy.ts ,reports,video

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,30 +1,43 @@
 import { defineConfig } from "cypress";
 
 
+
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
-    
-    baseUrl: "https://play1.automationcamp.ir",
+    baseUrl: "https://uitestingplayground.com",
+    //baseUrl: "https://play1.automationcamp.ir",
     setupNodeEvents(on, config) {
+      //reporter =cypress-mochawesome-reporter;
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
-    env:{
-       stage:'https://stage.pasv.us',
-       prod:'https://coding.pasv.us/course',
-       tekst:'Hello World!',
-       demoQA:'https://demoqa.com',
-       base:'https://uitestingplayground.com',
-       test:'https://play1.automationcamp.ir/expected_conditions.html',
+    env: {
+      stage: 'https://stage.pasv.us',
+      prod: 'https://coding.pasv.us/course',
+      tekst: 'Hello World!',
+      demoQA: 'https://demoqa.com',
+      base: 'https://uitestingplayground.com',
+      test: 'https://play1.automationcamp.ir/expected_conditions.html',
       email: "poletkhv2013@gmail.com",
       password: "Alisa992",
-      herokuapp:"https://the-internet.herokuapp.com"
+      herokuapp: "https://the-internet.herokuapp.com"
     },
     viewportWidth: 1200,
     viewportHeight: 1400,
   },
   retries: {
-    runMode: 2,
+    runMode: 3,
     openMode: 2,
   },
-  defaultCommandTimeout: 20_000,
+  video: true,
+  screenshotOnRunFailure: true,
+  defaultCommandTimeout: 16_000,
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'LecturePASV',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
 });
---
+++ cypress/e2e/debug.cy.ts
@@ -0,0 +1,20 @@
+
+describe('Login Page', () => {
+    beforeEach(() => {
+        cy.visit(
+        {
+            url:'click',
+            method:'GET'
+        }   
+        )
+    })
+    it('debug', () => {
+cy.get('#badButton').debug().click()
+    })
+    it.only('Pause', () => {
+       cy.pause()
+       cy.get('#badButton').click()
+       cy.pause()
+       cy.get('#badButton').should("have.css","background-color",'rgb(40, 167, 69)')
+})
+})
